4634d7fc4b4c314d47201498f71d561c2fe78fda
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-14 19:05:06 -0500

    HangImage.ts y App.tsx modificado, imágenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { letters } from "./helpers/letters";
+import { HangImage } from "./components/HangImage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       {/* Imágenes */}
-      <h3>Imagen del juego</h3>
+      <HangImage imageNumber={9} />
 
       {/* Palabra oculta */}
       <h3>_ _ _ _ _ _ _ _ _ _ _ </h3>
